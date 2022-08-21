da1bf744d3228689c680ebd6196b4b5a108055ab
Yoann Grudzien <yoann.grdz@gmail.com>
2022-08-21 23:31:56 +0200

    Added language selection dropdown menu.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,7 @@
 import React from "react";
+import uk from "../logos/uk.png";
+import jp from "../logos/japan.png";
+import fr from "../logos/france.png";
 
 export default function Navbar(){
 
@@ -11,6 +14,20 @@ export default function Navbar(){
                 <li className="nav--button"><a href="#Tools">My tools</a></li>
                 <li className="nav--button"><a href="#Projects">Projects</a></li>
                 <li className="contact--button">Get in touch</li>
+                <li className="language">
+                    <div className="flag--container">
+                        <img className="flag" src={uk} alt="english"></img>
+                    </div>
+                    <ul className="language--dropdown">
+                        <li className="language--space"></li>
+                        <li className="language--option">
+                            <img className="flag" src={fr} alt="english"></img>
+                        </li>
+                        <li className="language--option">
+                            <img className="flag" src={jp} alt="english"></img>
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </nav>
     )
